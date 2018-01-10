55e904ebdff5c0c980a7d161813d3eb428126c6f
mapforlisarae <35292850+mapforlisarae@users.noreply.github.com>
2018-01-10 13:02:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -425,7 +425,7 @@ gtag('event', 'discordlogModal');
     tbl = document.querySelector('#discordlogModal .logtable tbody');
     tbl.innerHTML = "";
     $.ajax('/log/' + 'discord' + '/' + d + '.json', {
-        dataType: 'text'
+        dataType: 'text', type: 'GET'
     }).done(function(text) {
         console.log(text);
         chatlog = JSON.parse(text);
